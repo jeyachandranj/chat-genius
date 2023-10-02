efb9a20f3ab419c7ee4a975a8a19fa8b33c6b2d5
jeyachandranj <125851197+jeyachandranj@users.noreply.github.com>
2023-10-02 19:12:32 +0530

    Revert "Revert "change title""
    This reverts commit 0fb110008205e218ca54c3ab1b7ddca8ee4c8465.

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -23,7 +23,7 @@ export default function Contacts({ contacts, changeChat }) {
         <Container>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h3>KONGU</h3>
+            <h3>CHAT GENIUS</h3>
           </div>
           <div className="contacts">
             {contacts.map((contact, index) => {
