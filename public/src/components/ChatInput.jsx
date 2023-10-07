20c8ebbf71cda3750ea998532ceff739d7db6ebf
jeyachandranj <125851197+jeyachandranj@users.noreply.github.com>
2023-10-07 22:36:33 +0530

    add translate,summary

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -28,6 +28,88 @@ export default function ChatInput({ handleSendMsg }) {
       setMsg("");
     }
   };
+  const [modal, setModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+  const toggleModal = (title) => {
+    
+    if(!modal)
+    {
+      let systemcontent=msg;
+      switch (title) {
+        case "TRANSLATE":
+          systemcontent="Please translate the given sentence into tamil "+msg;
+          break;
+        case "REPHRASE":
+          systemcontent="You are chatting with a friend. Rephrase and correct the grammar and spelling of the following message as if you were sending it to a friend";
+          break;
+        case "SUMMARY":
+          console.log("It's Wednesday!");
+          break;
+        case "EXPAND":
+          systemcontent="You are helping me expand short and incomplete sentences into meaningful messages, up to 100 words, as if I'm chatting with a friend";
+          break;
+        default:
+          console.log("It's the weekend!");
+      }
+      // Replace with your OpenAI API key
+      const apiKey = 'abikey'
+      const apiUrl = 'https://api.openai.com/v1/chat/completions';
+      const requestData = {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {role: "system", content: systemcontent},
+          { role: 'user', content: msg }],
+        temperature: 0.7,
+      };
+      fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify(requestData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the API response data here
+          if (data.choices && data.choices.length > 0) {
+            const content = data.choices[0].message.content;
+            switch (title) {
+              case "REPHRASE":
+                setMsg(content);
+                break;
+              default:
+            }
+            setModalData({
+              title: title, // Assuming your API response has a "title" field
+              content: data.choices[0].message.content, // Assuming your API response has a "content" field
+            });
+            console.log(content);
+          } else {
+            console.log("Invalid API response or no choices found.");
+            setModalData({
+              title: title, // Assuming your API response has a "title" field
+              content: "API not give respon please try again", // Assuming your API response has a "content" field
+            });
+          }
+          console.log("data",data);
+        })
+        .catch((error) => {
+          // Handle any errors that occur during the API request
+          setModalData({
+            title: title, // Assuming your API response has a "title" field
+            content: "exeption", // Assuming your API response has a "content" field
+          });
+        });
+    }
+    setModal(!modal);
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
 
   return (
     <Container>
@@ -37,16 +119,16 @@ export default function ChatInput({ handleSendMsg }) {
           {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
         </div>
         <div className="emoji">
-          <FcViewDetails />
+          <FcViewDetails onClick={() => toggleModal("REPHRASE")}/>
         </div>
         <div className="emoji">
-          <CiText />
+          <CiText onClick={() => toggleModal("TRANSLATE")} />
         </div>
         <div className="emoji">
-          <FcServiceMark />
+        <FcServiceMark onClick={() => toggleModal("SUMMARY")} />
         </div>
         <div className="emoji">
-          <FcSurvey />
+          <FcSurvey onClick={() => toggleModal("EXPAND")}/>
         </div>
       </div>
       <div style={{"padding-left":"150px"}}>
@@ -62,6 +144,20 @@ export default function ChatInput({ handleSendMsg }) {
         </button>
       </form>
       </div>
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>{modalData.title}</h2>
+            <p>
+              {modalData.content}
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
     </Container>
   );
 }
